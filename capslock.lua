19e780c849cbc1d77abe0d5f6a564f883b0f26b1
David <david.holton@outlook.com>
2018-10-26 09:48:35 -0400

    Saved 1 byte
    You must use Lua 5.3 for this to run. Do not use 5.1

--- capslock.lua
+++ capslock.lua
@@ -1,11 +1,11 @@
 --[[
-Input a string. Any "A" or "a" will change the capslock setting for the string
-Example:	AsTrAInG CAC
- Output:	StRInG Cc
+-- Input a string. Any "A" or "a" will change the capslock setting for the string
+-- Example:	AsTrAInG CAC
+--  Output:	StRInG Cc
 
-167 byte readable version:
-s=""
-a=0
+-- 171 byte readable version:
+a = 0
+s = ""
 io.read():gsub(".", function(c)
 	if c:find("[aA]") then
 		a = not a
@@ -16,8 +16,11 @@ io.read():gsub(".", function(c)
 end)
 print(s)
 
-138 byte one liner:
-s=""a=0 io.read():gsub(".",function(c)if c:find"[aA]"then a=not a else l=c:lower()s=s..(a and c or l==c and c:upper()or l)end end)print(s)
+-- 137 byte one liner:
+a=0s=""io.read():gsub(".",function(c)if c:find"[aA]"then a=not a else l=c:lower()s=s..(a and c or l==c and c:upper()or l)end end)print(s)
+
+This uses Lua 5.4
+
 --]]
 
-s=""a=0 io.read():gsub(".",function(c)if c:find"[aA]"then a=not a else l=c:lower()s=s..(a and c or l==c and c:upper()or l)end end)print(s)
+a=0s=""io.read():gsub(".",function(c)if c:find"[aA]"then a=not a else l=c:lower()s=s..(a and c or l==c and c:upper()or l)end end)print(s)
