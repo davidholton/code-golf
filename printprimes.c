07f0ee1687a4592e024d44fb70f4440811583361
David <david.holton@outlook.com>
2019-12-11 19:30:52 -0500

    Create printprimes.c

---
+++ printprimes.c
@@ -0,0 +1,14 @@
+/*
+  With an input of n through argv print out every prime number from 1-n on a newline.
+  Input: ./a.out 10
+  Output:
+  1
+  2
+  3
+  5
+  7
+  
+  121 bytes with gcc 8.3
+*/
+
+int i,n,j;main(int c,char**v){sscanf(v[1],"%d",&n);for(;i<n;i++){j=1;for(;i%++j&&j*j<i;);if(i<3?i:i%j)printf("%d\n",i);}}
