0ed5b04f4464942fc8e6329fc79136f01cc8e511
David <david.holton@outlook.com>
2019-12-11 19:34:52 -0500

    Update printprimes.c

--- printprimes.c
+++ printprimes.c
@@ -8,7 +8,7 @@
   5
   7
   
-  121 bytes with gcc 8.3
+  122 bytes with gcc 8.3
 */
 
 int i,n,j;main(int c,char**v){sscanf(v[1],"%d",&n);for(;i<=n;i++){j=1;for(;i%++j&&j*j<i;);if(i<3?i:i%j)printf("%d\n",i);}}
